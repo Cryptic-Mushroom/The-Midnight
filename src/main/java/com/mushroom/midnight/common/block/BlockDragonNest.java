2675e25d82b72a048ec26f412ee7fd0c01077361
Corail31 <m.pietquin@free.fr>
2018-12-21 15:34:56 +0100

    drip particle

--- src/main/java/com/mushroom/midnight/common/block/BlockDragonNest.java
+++ src/main/java/com/mushroom/midnight/common/block/BlockDragonNest.java
@@ -1,5 +1,6 @@
 package com.mushroom.midnight.common.block;
 
+import com.mushroom.midnight.client.particle.MidnightParticles;
 import com.mushroom.midnight.common.registry.ModBlocks;
 import com.mushroom.midnight.common.registry.ModEffects;
 import net.minecraft.block.Block;
@@ -9,6 +10,10 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 public class BlockDragonNest extends BlockGlowingPlant {
 
@@ -33,5 +38,14 @@ public class BlockDragonNest extends BlockGlowingPlant {
     public boolean canBlockStay(World world, BlockPos pos, IBlockState state) {
         Block blockUp = world.getBlockState(pos.up()).getBlock();
         return blockUp == ModBlocks.SHADOWROOT_LEAVES || blockUp == ModBlocks.DARK_WILLOW_LEAVES;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+        super.randomDisplayTick(state, world, pos, rand);
+        if (rand.nextInt(10) == 0) {
+            MidnightParticles.DRIP.spawn(world, (double) (pos.getX() + rand.nextFloat()), pos.getY() - 0.05D, (double) (pos.getZ() + rand.nextFloat()), 0d, 0d, 0d,191, 70, 82);
+        }
     }
 }
