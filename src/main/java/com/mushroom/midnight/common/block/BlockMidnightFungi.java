879803d8a507d200c4041d807b9ad2e3a2649f61
Corail31 <m.pietquin@free.fr>
2018-12-23 03:06:03 +0100

    bonemeal & mushrooms

--- src/main/java/com/mushroom/midnight/common/block/BlockMidnightFungi.java
+++ src/main/java/com/mushroom/midnight/common/block/BlockMidnightFungi.java
@@ -1,25 +1,19 @@
 package com.mushroom.midnight.common.block;
 
-import net.minecraft.block.BlockSapling;
+import com.mushroom.midnight.common.registry.ModBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.block.IGrowable;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
-import net.minecraftforge.event.terraingen.TerrainGen;
 
 import java.util.Random;
-import java.util.function.Supplier;
 
 public class BlockMidnightFungi extends BlockMidnightPlant implements IGrowable {
-    private final Supplier<WorldGenerator> generatorSupplier;
 
-    public BlockMidnightFungi(Supplier<WorldGenerator> generatorSupplier) {
-        super(true);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(BlockSapling.STAGE, 0));
-        this.generatorSupplier = generatorSupplier;
+    public BlockMidnightFungi() {
+        super(PlantBehaviorType.BUSH, true);
     }
 
     @Override
@@ -28,45 +22,27 @@ public class BlockMidnightFungi extends BlockMidnightPlant implements IGrowable 
     }
 
     @Override
-    public boolean canGrow(World world, BlockPos pos, IBlockState state, boolean isClient) {
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
         return true;
     }
 
     @Override
-    public boolean canUseBonemeal(World world, Random rand, BlockPos pos, IBlockState state) {
-        return world.rand.nextFloat() < 0.75F;
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        return true;
     }
 
     @Override
     public void grow(World world, Random rand, BlockPos pos, IBlockState state) {
-        if (state.getValue(BlockSapling.STAGE) == 0) {
-            world.setBlockState(pos, state.cycleProperty(BlockSapling.STAGE), 4);
-        } else if (TerrainGen.saplingGrowTree(world, rand, pos)) {
-            WorldGenerator generator = this.generatorSupplier.get();
-            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 4);
-            if (!generator.generate(world, rand, pos)) {
-                world.setBlockState(pos, state, 4);
-            }
+        Block grownBlock = null;
+        if (state.getBlock() == ModBlocks.NIGHTSHROOM) {
+            grownBlock = ModBlocks.DOUBLE_NIGHTSHROOM;
+        } else if (state.getBlock() == ModBlocks.DEWSHROOM) {
+            grownBlock = ModBlocks.DOUBLE_DEWSHROOM;
+        } else if (state.getBlock() == ModBlocks.VIRIDSHROOM) {
+            grownBlock = ModBlocks.DOUBLE_VIRIDSHROOM;
         }
-    }
-
-    @Override
-    public int damageDropped(IBlockState state) {
-        return 0;
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(BlockSapling.STAGE, meta & 1);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(BlockSapling.STAGE);
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, BlockSapling.STAGE);
+        if (grownBlock != null && grownBlock.canPlaceBlockAt(world, pos)) {
+            ((BlockMidnightDoubleFungi)grownBlock).placeAt(world, pos, 2);
+        }
     }
 }
