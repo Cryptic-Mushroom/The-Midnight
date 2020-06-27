66241af4a285b5ef0e3c0f885fb72d93c7e82d6b
Jonathan Colmenares <jonathing.projects@gmail.com>
2020-06-27 13:53:18 -0400

    Crash on world load for server

---
+++ MidnightMod/src/main/java/midnight/common/handler/BuildTestHandler.java
@@ -0,0 +1,24 @@
+package midnight.common.handler;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod.EventBusSubscriber(Dist.DEDICATED_SERVER)
+public class BuildTestHandler {
+    private static final Logger LOGGER = LogManager.getLogger("MidnightMod");
+
+    private BuildTestHandler() {
+    }
+
+    @SubscribeEvent
+    public static void serverStarted(FMLServerStartedEvent event) throws Exception {
+        LOGGER.info("The Midnight server started successfully. The game will now crash.");
+
+        throw new Exception();
+    }
+}
