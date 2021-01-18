74a158adb3e6aa37288c5b1841163c0ea5b8e59d
Jonathan Colmenares <jonathing.projects@gmail.com>
2021-01-18 17:27:48 -0500

    Simplified the funny

--- src/main/java/midnight/core/mixin/CrashReportMixin.java
+++ src/main/java/midnight/core/mixin/CrashReportMixin.java
@@ -17,6 +17,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -24,22 +25,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(CrashReport.class)
 public class CrashReportMixin {
-    @Shadow
-    @Final
-    private static Logger LOGGER;
-
     /**
      * Funny witty comment replacement for the Midnight because we need funny references. Will probably delete/disable
      * this on release.
      *
      * @param info Mixin's way of returning a result.
      */
-    @Inject(method = "getWittyComment()Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
-    private static void getWittyComment(CallbackInfoReturnable<String> info) {
-        String[] comments = {
+    @ModifyVariable(method = "getWittyComment()Ljava/lang/String;", at = @At("STORE"), index = 0)
+    private static String[] getWittyComment(String[] wittyComments) {
+        return new String[] {
             ":whythefuckisthisabug:",
             ":missingno:",
             ":nobrain:",
+            ":mlep:",
             "Error, brain not found.",
             "God damnit, Jonathing.",
             "We shouldn't have removed Dark Water.",
@@ -48,13 +46,8 @@ public class CrashReportMixin {
             "Ah fuck, not again.",
             "Voodoo magic ritual failed.",
             "Somebody asked for a 1.12.2 update again. Oh no.",
+            "Manually triggered debug crash",
             "Pro-tip: If you throw a geode at a rock, you'll get a NullPointerException!"
         };
-
-        try {
-            info.setReturnValue(comments[(int) (Util.nanoTime() % (long) comments.length)]);
-        } catch (Throwable throwable) {
-            LOGGER.error("Couldn't get a funny comment for the Midnight crashing. Continuing with a normal witty comment.", throwable);
-        }
     }
 }
